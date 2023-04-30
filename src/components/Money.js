d4414d555aa7776667f900fced62bbda7d73c2aa
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-04-30 12:02:31 +0200

    feat: adding pot autoincrementer

--- src/components/Money.js
+++ src/components/Money.js
@@ -1,12 +1,32 @@
+import { useState } from "react";
+
 export default function Money() {
-  const myMoney = 1000;
-  const yourMoney = 1000;
+  let [pot, setPot] = useState(0);
+  let [initialPot, setInitialPot] = useState(0);
+  let [myMoney, setMymoney] = useState(1000);
+  let [yourMoney, seYourMoney] = useState(1000);
+
+  function potIncrementer() {
+    setPot(pot + 30);
+  }
+
+  function varyPot() {
+    setInitialPot(pot);
+    setMymoney(myMoney - pot / 3);
+    seYourMoney(yourMoney - (pot / 3) * 2);
+  }
+
+  setInterval(potIncrementer, 3000);
+
   return (
-    <div className="flex flex-col justify-between items-center absolute p-1 px-3 left-5 top-0 py-5 h-full">
+    <div className="flex flex-col justify-between absolute p-1 px-3 left-5 top-0 py-5 h-full text-center">
       <div className="border-2 border-black p-1 px-3">
-        Money : {yourMoney} $
+        Opponent Money : {yourMoney} $
       </div>
-      <div className="border-2 border-black p-1 px-3">Money : {myMoney} $</div>
+      <div className="border-2 border-black p-1 px-3">Pot : {initialPot} $</div>
+      <div className="border-2 border-black p-1 px-3" onClick={varyPot}>
+        Your Money : {myMoney} $
+      </div>
     </div>
   );
 }
